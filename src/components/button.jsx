e1a4f22c02f473651664e1fd2e604bc5db5ed402
xzcxvc <daryl.tiamps@gmail.com>
2025-11-09 02:21:50 +0800

    wip input and combobox component for filters

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-export default function Button({ children, ...props }) {
+export default function Button({ children, color, ...props }) {
   return (
     <button
       {...props}
-      className="bg-crimson-900 font-semibold p-3 rounded-full 
-          hover:translate-y-[-1px] hover:transition-all hover:duration-100 hover:bg-crimson-1000"
+      className={`${
+        color === "crimson"
+          ? "bg-crimson-900 hover:bg-crimson-1000 text-black"
+          : "bg-white text-crimson-1000 border-0"
+      },
+        font-semibold p-3 rounded-full hover:translate-y-[-1px] 
+        hover:transition-all hover:duration-100 `}
     >
       {children || "Button"}
     </button>
