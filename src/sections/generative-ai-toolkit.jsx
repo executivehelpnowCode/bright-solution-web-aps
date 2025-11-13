d3c4a10b3009ff330e45f4b501a947acb1b7fb23
samaniegolouie <48710475+samaniegolouie@users.noreply.github.com>
2025-11-13 16:23:39 +0800

    update the card component for icon

--- src/sections/generative-ai-toolkit.jsx
+++ src/sections/generative-ai-toolkit.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../components/card";
-import { generativeAiToolkit } from "../ constants";
+import { generativeAiToolkit } from "../ constants"; 
 
 export default function GenerativeAiToolkit() {
   return (
@@ -8,12 +8,23 @@ export default function GenerativeAiToolkit() {
       {generativeAiToolkit.map((report, index) => (
         <article key={index}>
           <Card
+            icon={
+              report.icon
+                ? React.createElement(report.icon, {
+                    className: "w-4 h-4 text-red-900",
+                  })
+                : null
+            }
             title={report.title}
+            titleSize="text-[16px]" 
             titleColor="text-red-950"
             description={report.description}
             className="flex flex-col justify-between w-full h-full hover:-translate-y-1 hover:shadow-[0px_13px_15px_5px_rgba(0,0,0,0.1)] transition-all duration-200 rounded-xl border border-gray-200 bg-white p-6"
           >
-            <a href={report.url} class="font-semibold text-red-700 hover:none">
+            <a
+              href={report.url}
+              className="font-semibold text-red-700 hover:underline"
+            >
               {report.label}
             </a>
           </Card>
